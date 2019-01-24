a02438023c017fb4e18aa7497acac477e8da5edf
zekidok <zekidok@gmail.com>
2019-01-24 11:48:58 +0100

    leo suce

---
+++ lib/create_board.rb
@@ -0,0 +1,16 @@
+def puts_board_V2
+  puts "   " + "1" + "   " + "2" + "   " + "3" + "  "
+  puts " +---+---+---+"
+  puts " |   |   |   |"
+  puts "A| " + @board[0] + " | " + @board[1] + " | " + @board[2] + " |"
+  puts " |   |   |   |"
+  puts " +---+---+---+"
+  puts " |   |   |   |"
+  puts "B| " + @board[3] + " | " + @board[4] + " | " + @board[5] + " |"
+  puts " |   |   |   |"
+  puts " +---+---+---+"
+  puts " |   |   |   |"
+  puts "C| " + @board[6] + " | " + @board[7] + " | " + @board[8] + " |"
+  puts " |   |   |   |"
+  puts " +---+---+---+"
+end
